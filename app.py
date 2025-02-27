b9e43150b7cfa91c96cd7cf91f0d19a6cd6d32f5
AliceLoraine <aliceloraine.a@gmail.com>
2025-02-27 07:28:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,3 +13,12 @@ text = st.text_input('write something', 'this is my text')
 st.write('the text that has been written is:', text)
 
 st.subheader('now, we use 2 columns')
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("This is a column")
+  st.write("Shadow is my Religion")
+  resp = st.checkbox('I concur')
+  if resp:
+    st.write('Good...Very Good...')
