00edb4b9c8bc3f7a4158f3d5f54de7d0f6fbb7e3
AliceLoraine <aliceloraine.a@gmail.com>
2025-02-27 07:23:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,8 @@ st.write("He is the ultimate lifeform")
 image = Image.open('shads.png')
 
 st.image(image, caption = 'youre welcome')
+
+text = st.text_input('write something', 'this is my text')
+st.write('the text that has been written is:', text)
+
+st.subheader('now, we use 2 columns')
