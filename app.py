a23ef610de8dddd4f4ada1258e35a414d1caf5b8
AliceLoraine <aliceloraine.a@gmail.com>
2025-02-27 07:10:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from PIL import image
 
 st.title("My First App")
+
+st.header("Here is Shadow The Hedgehog")
+st.write("He is the ultimate lifeform")
+image = Image.open('in
